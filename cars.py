0885b101d77e8fbeb3dfc774a61c5a5341c3f1c0
NIVEDAZLS <101988753+NIVEDAZLS@users.noreply.github.com>
2022-04-15 18:37:43 +0530

    Update cars.py

--- cars.py
+++ cars.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 import streamlit as st
-import psycopg2-binary
+import psycopg2
 import postgreconnect
 
 #Define a sql to fetch data from CARS table (PostgreSQL table hosted on Heroku)
